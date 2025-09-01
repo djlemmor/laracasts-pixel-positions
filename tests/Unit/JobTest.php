10d1e76c5f4e5348f5b8084aebced540216dbd0c
DJ Lemmor Nuique <lemmornuique@gmail.com>
2025-09-02 03:35:42 +0800

    Jobs, Tags, TDD

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Job;
+
+test("it belongs to an employer", function () {
+    /* AAA */
+
+    // Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        "employer_id" => $employer->id,
+    ]);
+
+    // Act & Assert
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+test("it can have tags", function () {
+    /* AAA */
+
+    // Arrange
+    $job = Job::factory()->create();
+
+    // Act
+    $job->tag('Frontend');
+    
+    // Assert
+    expect($job->tags)->toHaveCount(1);
+});
